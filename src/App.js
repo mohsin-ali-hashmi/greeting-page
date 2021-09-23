d6c4d9e8e60bc195e42dfd67a2464b8e5d8af415
Mohsin Hashmi <mohsinalihashmi.7@gmail.com>
2021-09-23 22:46:37 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    let curDate = new Date();
+    curDate = curDate.getHours();
+
+    let greeting = '';
+    let cssStyle = {};
+
+    if (curDate >= 1 && curDate < 12) {
+        greeting = 'Good Morning';
+        cssStyle.color = "Yellow";
+    } else if (curDate >= 12 && curDate <= 19) {
+        greeting = 'Good Afternoon';
+        cssStyle.color = "orange";
+    } else {
+        greeting = 'Good Night';
+        cssStyle.color = "blue";
+    }
+
+
+    return (
+        <div className="mydiv">
+            <h1> Hello Sir , <span style={cssStyle}>{greeting}</span> </h1>
+        </div>
+    )
+}
+
+export default App
